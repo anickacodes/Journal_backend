c18a38031177d6c15539e1e971ed98455519bf34
anickacodes <anickalewis@pursuit.org>
2023-11-30 16:53:49 -0500

    render mongoose troubleshoot

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 const usersRouter = express.Router();
 const User = require("../models/User");
 
@@ -16,8 +18,8 @@ usersRouter.get("/", async (req, res) => {
 usersRouter.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    const user = await User.findById(id, "username email");
-    console.log(user)
+    const user = await User.findById(id, "username email password");
+    console.log(user);
     if (!user)
       res
         .status(404)
@@ -43,21 +45,39 @@ usersRouter.post("/register", async (req, res) => {
     //       .json({ message: "Username or email already exists" });
     //   }
 
-    //   const saltRounds = 10;
-    //   const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    // const user = new User({ username, email, password });
-    // console.log("user before await", user);
-    const user = await User.create({ username, email, password });
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
     res.status(201).json({ message: "User registered successfully", ...user });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: error });
+    res.status(500).json({ message: "Error registering user", error });
   }
 });
 
 usersRouter.post("/login", async (req, res) => {
-  // user login logic
-});
+    const { username, password } = req.body;
+    console.log("Received login request body:", { username, password })
+//   const secretKey = crypto.randomBytes(32).toString("hex");
+//   console.log(secretKey);
+const user = await User.findOne({ username });
+console.log("Found user:", user);
+
+if (user && (await bcrypt.compare(password, user.password))) {
+  const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+  console.log("Generated Token:", token);
+  res.status(200).json({ message: "Login successful", user, token });
+} else {
+  res.status(401).json({ message: "Invalid username or password" });
+}
+})
 
 module.exports = usersRouter;
